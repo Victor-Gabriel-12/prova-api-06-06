da812731f584ec7f34aa8c729a8314cbed0141ea
Victor Gabriel Rodrigues Silva <vg9469187@gmail.com>
2022-06-06 10:54:42 -0300

    inicialize

--- api/src/repository/petrepository.js
+++ api/src/repository/petrepository.js
@@ -2,9 +2,14 @@ import { con } from './connection.js'
 
 
 
-export async funcion inserirPet(pet){
+export async function inserirPet(pet){
     const comando = `select tb_pet
                       `
 
+}
+
+export async function cosultarPet(pet){
+    const comando = `
+                        `
 }
 
